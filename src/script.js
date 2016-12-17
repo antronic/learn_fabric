21204a46ef9bdd797a802eca15d1c0b8b411f4dd
antronic <antronic.inc@gmail.com>
2016-12-18 02:33:27 +0700

    Finished lesson 2, 3, and Lesson can draw

--- src/script.js
+++ src/script.js
@@ -1,9 +1,20 @@
-const canvas = new fabric.Canvas('c1')
+const canvas1 = new fabric.Canvas('c1')
+const canvas2 = new fabric.Canvas('c2')
+const canvas3 = new fabric.Canvas('c3')
 
 var rect = new fabric.Rect({
   left: 100,
   top: 10,
   fill: '#f0f',
+  width: 50,
+  height: 50,
+  angle: 45,
+})
+
+var rect2 = new fabric.Rect({
+  left: 100,
+  top: 10,
+  fill: '#00f',
   width: 50,
   height: 50,
   angle: 45,
@@ -16,11 +27,43 @@ var circle = new fabric.Circle({
   radius: 25,
 })
 
+var circle2 = new fabric.Circle({
+  left: 30,
+  top: 70,
+  fill: '#ff0',
+  radius: 25,
+})
+
 fabric.Image.fromURL('XdQULww6_400x400.png', (objImg) => {
   objImg.scale(0.25)
   objImg.top = 140
   objImg.left = 50
-  canvas.add(objImg)
+  canvas1.add(objImg)
 })
 
-canvas.add(rect, circle)
+canvas1.add(rect, circle)
+
+rect.animate('left', '+=100', {
+  onChange: canvas2.renderAll.bind(canvas2),
+  // on canvas2 renderAll finish rect that bind on (canvas2) will play
+  duration: 1000,
+  easing: fabric.util.ease.easeOutBounce,
+})
+
+var str = 'This is a book.'
+var text = new fabric.Text(str, {
+  top: 200,
+  left: 200,
+  fontSize: 14,
+})
+
+canvas2.add(rect, circle, text)
+
+var group = new fabric.Group([circle2, rect2], {
+  top: 100,
+  left: 40,
+})
+
+canvas3.isDrawingMode = true
+
+canvas3.add(group)
